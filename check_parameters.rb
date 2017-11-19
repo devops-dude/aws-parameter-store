d8d27d97d92ef05a4e632ebf4b39b61899b1a2e3
Doug Smith <dsmithatx@gmail.com>
2017-11-19 14:32:47 -0600

    convert simple if else to a ternary

--- check_parameters.rb
+++ check_parameters.rb
@@ -11,16 +11,14 @@ parameters = YAML.load_file(File.join(__dir__, 'parameters.yml'))
 parameters.each do |parameter_name, values|
   type = values[0]
 
-  # puts type
-  if type == 'SecureString'
-    with_decryption = true
-  elsif type == 'String'
-    with_decryption = false
-  end
+  # if type is SecureString set with_decryption to true
+  # true or false is required by the AWS CLI to lookup a parameter
+  type == 'SecureString' ? with_decryption = true : false
 
   begin
-    # use ps module and put parameter method to create parameters
+    # use ps module and put parameter method to fetch parameters
     puts PS.get_parameter(parameter_name, with_decryption)
+  # error handling AWS CLI output
   rescue Aws::SSM::Errors::ServiceError => e
     puts 'Error Fetching Parameter: ' + parameter_name
     puts "#{e.class} #{e.message}"
