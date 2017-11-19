29567faa4587a15c65cc5b2be675a3a16e5b39b2
Doug Smith <dsmithatx@gmail.com>
2017-11-19 00:46:30 -0600

    fix msgs

--- lib/ps.rb
+++ lib/ps.rb
@@ -15,20 +15,20 @@ module PS
     return resp.parameter.value
   end
 
-  def self.put_parameter(parameter_name, description, value, type)
+  def self.put_parameter(parameter_name, type, description, value)
     @client.put_parameter(
       name: parameter_name,
       description: description,
       value: value,
       type: type
     )
-    puts "#{parameter_name} was created.\n"
+    puts "Parameter #{parameter_name} was created.\n"
   end
 
   def self.delete_parameter(parameter_name)
     @client.delete_parameter(
       name: parameter_name
     )
-    puts "#{parameter_name} was deleted.\n"
+    puts "Parameter #{parameter_name} was deleted.\n"
   end
 end
